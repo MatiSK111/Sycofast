cfed9467196edabb49ef30290750e31765978c3c
Matias <92665389+MatiSK111@users.noreply.github.com>
2022-06-07 20:38:58 -0400

    Estilo al editar

--- Listar_usu.php
+++ Listar_usu.php
@@ -7,62 +7,85 @@ $result=mysqli_query(conexion(), $sql);
 <!DOCTYPE html>
 <html>
 <head>
-<style>
-table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
 
-td, th {
-  border: 1px solid #dddddd;
-  text-align: left;
-  padding: 8px;
-}
 
-tr:nth-child(even) {
-  background-color: #dddddd;
-}
-</style>
+<!--Validaciones-->
+<script src="Javascript/validar_actualizar.js"></script>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- Primary Meta Tags -->
+<title>Psicofast</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="title" content="Psicofast">
+<meta name="author" content="Themesberg">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<!-- Pixel CSS -->
+<link rel="icon" type="image/png" sizes="16x16" href="./assets/img/favicon/Psicofastlogoround.png">
+
+
+<link type="text/css" href="./css/neumorphism.css" rel="stylesheet">
+<!-- Validaciones -->
+
+<!-- Alertas facheritas -->
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 </head>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+<body >
+<div  style="padding-bottom:800px">
 
-<table>
-  <tr>
-    <th>Rut</th>
-    <th>Nombre</th>
-    <th>Apellidos</th>
-    <th>Fecha de nacimiento</th>
-    <th>Direccion</th>
-    <th>Telefono</th>
-    <th>Email</th>
-    <th>Estado</th>
-   
-  </tr>
-  
+
+
+<form name="form1" action="procesar_actualizar.php" method="POST" enctype="multipart/form-data" >
+<div class="section  text-dark section-lg" >
+
+        <div class="row justify-content-center" >
+            <div class="col-lg-10" >
+            <center>
+            <h1>Editar Pacientes</h1>
+            <br>
+            </center>
+                <div class="mb-5">
+                    <table class="table shadow-soft rounded">
+                        <tr>
+                            <th class="border-0" scope="col">Rut</th>
+                            <th class="border-0" scope="col">Nombre</th>
+                            <th class="border-0" scope="col">Apellidos</th>
+                            <th class="border-0" scope="col">Fecha de nacimiento</th>
+                            <th class="border-0" scope="col">Direccion</th>
+                            <th class="border-0" scope="col">Telefono</th>
+                            <th class="border-0" scope="col">Email</th>
+                            <th class="border-0" scope="col">Estado</th>
+                        </tr>
+                        
    <?php
    while($datos=mysqli_fetch_array($result)){
    ?>
-    <tr>
-    <td><?php echo $datos['rut']?></td>
-    <td><?php echo $datos['nombres']?></td>
-    <td><?php echo $datos['Apellidos']?></td>
-    <td><?php echo $datos['fechanac']?></td>
-    <td><?php echo $datos['direccion']?></td>
-    <td><?php echo $datos['telefono']?></td>
-    <td><?php echo $datos['mail']?></td>
-    <td><?php if ($datos['estado']==1){
-                echo "Activo";
-             }else{
-               echo "Inactivo";
-             }
     
-    ?></td>
-    <td><a href="Editar_usu.php?Rut=<?php echo $datos['rut']?>"class="btn btn-lg btn-primary3" type="button"><pre>Editar</a></td>
-    <td><a href="Eliminar_usu.php?Rut=<?php echo $datos['rut']?>"class="btn btn-lg btn-primary3" type="button"><pre>Eliminar</a></td>
+
+<tr>
+                            <td><?php echo $datos['rut']?></td>
+                            <td><?php echo $datos['nombres']?></td>
+                            <td><?php echo $datos['Apellidos']?></td>
+                            <td><?php echo $datos['fechanac']?></td>
+                            <td><?php echo $datos['direccion']?></td>
+                            <td><?php echo $datos['telefono']?></td>
+                            <td><?php echo $datos['mail']?></td>
+                            <td><?php if ($datos['estado']==1){
+                                        echo "Activo";
+                                    }else{
+                                      echo "Inactivo";
+                                    }
+                            
+                            ?></td>
+                            <td><a href="Editar_usu.php?Rut=<?php echo $datos['rut']?>"class="btn btn-primary" type="button">Editar</a></td>
+                            <td><a href="Eliminar_usu.php?Rut=<?php echo $datos['rut']?>"class="btn btn-primary" type="button">Eliminar</a></td>
     
-    
-  </tr>
-  
+                        </tr>
    <?php
    } ?>
 </table>
+</center>
