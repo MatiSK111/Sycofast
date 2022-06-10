c60854b7ebb3ff18d97f6de3a920e33cccf8b13c
gercarvajal <nikocarvajal1996@gmail.com>
2022-06-10 11:09:02 -0400

    s,ajdnksja

--- procesaregistro.php
+++ procesaregistro.php
@@ -7,13 +7,14 @@ include("functions/setup.php");
 //$sql="insert into propietarios ='".$_POST['frmusuario']."' and clave='".md5($_POST['frmclave'])."' and estado=1";
 //echo $sql;
 //die; cieere de ejecución hace un stop
-$sql="insert into usuario values('".$_POST['rut']."','".$_POST['nombres']."','".$_POST['apellidos']."','".$_POST['user']."','".$_POST['fechanac']."','".$_POST['direccion']."','".md5($_POST['pass'])."','paciente','".$_POST['estado']."','".$_POST['telefono']."','".$_POST['correo']."')";
+$sql="insert into usuario values('".$_POST['rut']."','".$_POST['nombres']."','".$_POST['apellidos']."','".$_POST['user']."','".$_POST['fechanac']."','".$_POST['direccion']."','".md5($_POST['pass'])."','paciente',".$_POST['estado'].",'".$_POST['telefono']."','".$_POST['correo']."')";
 
 
 
 
 $sql2="select * from usuario where rut='".$_POST['rut']."' ";
-
+echo $sql;
+die;
 
 
 $result=mysqli_query(conexion(),$sql2);
